922fa987391407451d0fc3fcc47fc0af3627db00
CreativeWolf <creativewolf@gmail.com>
2015-07-22 16:54:53 +0530

    V2

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
 
-	"database": "mongodb://root:abc123@ds045031.mongolab.com:45031/userstory",
+	"database": "mongodb://cw:123456@ds055822.mongolab.com:55822/storify",
 	"port": process.env.PORT || 3000,
-	"secretKey": "YourSecretKey"
+	"secretKey": "StorifyRocks"
 
 }
